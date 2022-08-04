97bb3275e04b61b9a4655b4385659425b89bd7cc
Camila <camila.matosrocha1234@gmail.com>
2022-08-04 18:31:38 -0300

    Primeira versão dos projetos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SobreMimComponent } from './sobre-mim/sobre-mim.component';
+import { ProjetosComponent } from './projetos/projetos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SobreMimComponent
+    SobreMimComponent,
+    ProjetosComponent
   ],
   imports: [
     BrowserModule
